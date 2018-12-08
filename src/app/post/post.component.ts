c28ba909146d92ef93a0fbd177da75f387b4201a
pras75299 <singh.prashantking@gmail.com>
2018-12-09 00:10:46 +0530

    slightly change  in package json and post service

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -11,37 +11,64 @@ export class PostComponent implements OnInit {
     }
     ngOnInit() {
       this.service.getPosts()
-    .subscribe(Response => {
+    .subscribe(
+      Response => {
       this.posts = Response.json();
        // console.log(Response.json());
-    });
-   }
+      },
+      error => {
+        alert('An unexpected error occured');
+        console.log(error);
+      });
+    }
 
    createPost(input: HTMLInputElement) {
      // const post: any = {title: input.value};
      const post = {title: input.value};
      input.value = '';
       this.service.createPostServ(post)
-      .subscribe(Response => {
+      .subscribe(
+        Response => {
         post['id'] = Response.json().id;
         this.posts.splice(0, 0, post);
         // console.log(Response.json());
+      },
+      (error: Response) => {
+        if (error.status === 400) {
+          // this.form.setErrors(error.json());
+        } else {
+            alert('An unexpected error occur in createPost.');
+            console.log(error);
+        }
       });
    }
 
    updatePost(post) {
       this.service.updatePostServ(post)
-      .subscribe(Response => {
+      .subscribe(
+        Response => {
         console.log(Response.json());
+      },
+      error => {
+        alert('An unexpected error occurs in update post request.');
       });
       // this.http.put(this.url, JSON.stringify(post))
    }
 
    deletePost(post) {
-     this.service.deletePostServ(post)
-     .subscribe(Response => {
+     this.service.deletePostServ(post.id)
+     .subscribe(
+       Response => {
        const index = this.posts.indexOf(post);
        this.posts.splice(index, 1);
+     },
+     (error: Response) => {
+       if (error.status === 404) {
+          alert('this post already has been deleted');
+       } else {
+          alert('An unexpected error occues in deletepost request.');
+          console.log(error);
+       }
      });
    }
 
--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,6 +1,5 @@
 import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
-
 @Injectable({
   providedIn: 'root'
 })
@@ -19,7 +18,11 @@ export class PostService {
    return this.http.patch(this.url + '/' + post.id, JSON.stringify({ isRead: true}));
   }
 
-  deletePostServ(post) {
-   return this.http.delete(this.url + '/' + post.id);
-  }
+  // deletePostServ(post) {
+  //  return this.http.delete(this.url + '/' + post.id);
+  // } // we are passing the entire post object when we just need the id to delete it
+
+  deletePostServ(id) {
+    return this.http.delete(this.url + '/' + id);
+   }
 }
