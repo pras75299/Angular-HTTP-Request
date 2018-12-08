37e3058d8259960826a7e3f4635ea49d1189bb89
pras75299 <singh.prashantking@gmail.com>
2018-12-08 16:36:48 +0530

    added http methods for post component, now the idea of sepration of concern has been fullfilled

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,9 +1,25 @@
+import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
+  private url = 'https://jsonplaceholder.typicode.com/posts';
+  constructor(private http: Http) { }
+  getPosts() {
+    return this.http.get(this.url);
+  }
 
-  constructor() { }
+  createPostServ(post) {
+    return this.http.post(this.url, JSON.stringify(post));
+  }
+
+  updatePostServ(post) {
+   return this.http.patch(this.url + '/' + post.id, JSON.stringify({ isRead: true}));
+  }
+
+  deletePostServ(post) {
+   return this.http.delete(this.url + '/' + post.id);
+  }
 }
